a79c029265223564ece2a015677380e61116f59f
kalom60 <39136564+kalom60@users.noreply.github.com>
2023-06-06 11:49:02 +0300

    configuring prisma to connect to mongo atlas

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 import express from "express";
 import cors from "cors";
 
+import prisma from "./prisma/prisma";
+
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`App running at port ${PORT}`);
-});
+async function checkDBConnection() {
+  try {
+    await prisma.$connect();
+    app.listen(PORT, () => {
+      console.log(`App running at port ${PORT}`);
+    });
+  } catch (err) {
+    console.log("DB is not connected!!!");
+    process.exit(1);
+  }
+}
+
+checkDBConnection();
