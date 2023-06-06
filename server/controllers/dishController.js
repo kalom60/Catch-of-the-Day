f5a099f1d3b94c446fe7d148891c9a1b26318410
kalom60 <39136564+kalom60@users.noreply.github.com>
2023-06-06 12:41:35 +0300

    adding DishController methods

--- server/controllers/dishController.js
+++ server/controllers/dishController.js
@@ -1,5 +1,25 @@
 import DishModel from "../models/dishModel";
 
-class DishController {}
+class DishController {
+  static async createDish(req, res) {
+    const dish = await DishModel.createDish(req.body);
+    res.json(dish);
+  }
+
+  static async getAllDishes(req, res) {
+    const dishes = await DishModel.getAllDishes();
+    res.json(dishes);
+  }
+
+  static async updateDish(req, res) {
+    const dish = await DishModel.updateDish(req.params, req.body);
+    res.json(dish);
+  }
+
+  static async deleteDish(req, res) {
+    const dish = await DishModel.deleteDish(req.params);
+    res.json(dish);
+  }
+}
 
 export default DishController;
