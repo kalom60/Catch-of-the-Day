6f82769825c31d39714d126448caf6e0b6f986e3
kalom60 <39136564+kalom60@users.noreply.github.com>
2023-06-06 12:32:03 +0300

    adding dishModel methods

--- server/models/dishModel.js
+++ server/models/dishModel.js
@@ -1,5 +1,50 @@
 import prisma from "../prisma/prisma";
 
-class DishModel {}
+class DishModel {
+  static async createDish(value) {
+    try {
+      const dish = await prisma.dishes.create({
+        data: value,
+      });
+      return dish;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async getAllDishes() {
+    try {
+      const dishes = await prisma.dishes.findMany();
+      return dishes;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async updateDish(id, value) {
+    try {
+      const dish = await prisma.dishes.update({
+        where: {
+          id,
+        },
+        data: value,
+      });
+      return dish;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async deleteDish(id) {
+    try {
+      const dish = await prisma.dishes.delete({
+        where: { id },
+      });
+      return dish;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
 
 export default DishModel;
