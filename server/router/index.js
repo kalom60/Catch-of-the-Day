78c48260b43ee451714337d85b7c111b9145d66c
kalom60 <39136564+kalom60@users.noreply.github.com>
2023-06-06 12:43:25 +0300

    adding api routes

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import DishController from "../controllers/dishController";
 
 const router = express.Router();
+
+router
+  .route("/")
+  .get(DishController.getAllDishes)
+  .post(DishController.createDish);
+router
+  .route("/:id")
+  .patch(DishController.updateDish)
+  .delete(DishController.deleteDish);
 
 export default router;
